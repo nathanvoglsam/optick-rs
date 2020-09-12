13d054d137c246dab58ae6c084aaf204a0098c67
Nathan Voglsam <nathanvoglsam420@gmail.com>
2020-09-12 18:06:27 +1000

    Initial msvc build support

--- build.rs
+++ build.rs
@@ -1,18 +1,29 @@
-fn runtime() -> &'static str {
-    if std::env::var("CARGO_CFG_TARGET_FEATURE")
+extern crate cmake;
+
+use std::path::PathBuf;
+
+///
+/// Are we building for the static CRT on Windows MSVC
+///
+fn static_crt() -> bool {
+    std::env::var("CARGO_CFG_TARGET_FEATURE")
         .unwrap_or_default()
         .contains("crt-static")
-    {
-        "MT"
-    } else {
-        "MD"
-    }
 }
 
 fn main() {
-    println!(
-        "cargo:rustc-link-search={}/external/lib/x64/",
-        std::env!("CARGO_MANIFEST_DIR")
-    );
-    println!("cargo:rustc-link-lib=OptickCore_{}", runtime())
+    let path: PathBuf = [std::env!("CARGO_MANIFEST_DIR"), "external", "optick"].iter().collect();
+
+    let mut config = cmake::Config::new(path);
+    config.define("OPTICK_STATIC", "ON");
+    config.static_crt(static_crt());
+
+    let out = config.build().join("lib");
+    let out = out.to_str().unwrap();
+    println!("cargo:rustc-link-search=native={}", &out);
+
+    println!("cargo:rustc-link-lib=static=OptickCored");
+    println!("cargo:rustc-link-lib=stdc++");
+    println!("cargo:rustc-link-lib=ws2_32");
+    println!("cargo:rustc-link-lib=dbghelp");
 }
